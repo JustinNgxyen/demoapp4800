4ffbaaf68d49290c78d6372e20789007b1cee332
Justin Nguyen <justinnguyen2026@gmail.com>
2026-02-19 20:41:55 -0800

    test change

--- server.py
+++ server.py
@@ -32,4 +32,4 @@ def search_food_items(budget):
     return Response(dumps(foods), mimetype="application/json")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5005, debug=True)
+    app.run(host="0.0.0.0", port=5005, debug=False)
